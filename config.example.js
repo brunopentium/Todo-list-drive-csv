3fbf6ba421d5822813df09d7510f9eb5cda6bef9
brunopentium <brunocesardesouza26@gmail.com>
2026-06-23 15:52:49 -0300

    Add config example

---
+++ config.example.js
@@ -0,0 +1,5 @@
+window.TASKMASTER_CONFIG = {
+  googleClientId: 'COLE_SEU_CLIENT_ID_AQUI.apps.googleusercontent.com',
+  driveScope: 'https://www.googleapis.com/auth/drive.file',
+  defaultCsvName: 'taskmaster-drive.csv'
+};
